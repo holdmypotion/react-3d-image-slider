7b3ec87a44ee4e3a3c3d1cae5ae4f114ed778732
holdmypotion <holdmypotion@gmail.com>
2021-02-13 22:53:53 +0530

    Working model complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ImageSlider from "./components/ImageSlider";
+
+import { IMAGES, VIDEOS, LARGE_IMAGES } from "./data/data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <ImageSlider images={IMAGES} slidesToShow={5} />
+      </div>
+      <div className='container'>
+        <ImageSlider images={VIDEOS} />
+      </div>
+      <div className='container'>
+        <ImageSlider images={LARGE_IMAGES} />
+      </div>
     </div>
   );
 }
---
+++ src/components/ImageSlider.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import Slider from "react-slick";
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
+
+import "./ImageSlider.css";
+
+const NextArrow = ({ onClick }) => {
+  return (
+    <div className='nextArrow' onClick={onClick}>
+      <BsChevronRight />
+    </div>
+  );
+};
+
+const PrevArrow = ({ onClick }) => {
+  return (
+    <div className='prevArrow' onClick={onClick}>
+      <BsChevronLeft />
+    </div>
+  );
+};
+
+const ImageSlider = ({ images, slidesToShow = 3 }) => {
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const settings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    dots: false,
+    speed: 300,
+    slidesToShow: slidesToShow,
+    centerPadding: "0",
+    swipeToSlide: true,
+    focusOnSelect: true,
+    nextArrow: <NextArrow onClick />,
+    prevArrow: <PrevArrow onClick />,
+    beforeChange: (current, next) => setImageIndex(next),
+    responsive: [
+      {
+        breakpoint: 1490,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 820,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  const templateImages = images.map((image, idx) => {
+    if (image !== null) {
+      return (
+        <div
+          className={idx === imageIndex ? "activeSlide" : "slide"}
+          key={image.id}
+        >
+          <div className='slideWrapper'>
+            {image.code ? image.code : <img src={image.src} alt={image.alt} />}
+          </div>
+        </div>
+      );
+    }
+    return null;
+  });
+
+  return <Slider {...settings}>{templateImages}</Slider>;
+};
+
+export default ImageSlider;
